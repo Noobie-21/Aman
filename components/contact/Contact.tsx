9d2a56e2fe21e154b486a3cca36971cb2a79e310
noobie <amangupta807646@gmail.com>
2023-08-01 23:43:40 +0530

    fix some issues

--- components/contact/Contact.tsx
+++ components/contact/Contact.tsx
@@ -5,12 +5,12 @@ type Props = {};
 
 const Contact = (props: Props) => {
   return (
-    <div className="w-full md:h-72 h-full p-6 flex flex-col justify-center items-center mt-6 ">
+    <div className="w-full md:h-72 h-full p-6 flex flex-col justify-center items-center mt-6 text-slate-100">
       <div className="flex flex-col justify-center items-center w-full h-full ">
         <h1 className="md:text-7xl text-5xl font-mulish font-bold bg-gradient-to-r from-violet-800  to-violet-600  text-transparent bg-clip-text mb-6">
           Get in Touch
         </h1>
-        <h4 className="max-w-xl text-2xl font-roboto text-center mb-5">
+        <h4 className="max-w-xl text-2xl font-roboto text-center mb-5 ">
           Thanks for checking out my portfolio 🎉! I hope you liked it, I would
           appreciate your feedback also, I'm constantly looking for new
           opportunities and welcome anyone to my inboxes ✉️.
